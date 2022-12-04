2370869daa3a10958ccb813eb22c63fbfdb7d88d
Kabir Deula <kabirdeula167@gmail.com>
2022-12-04 16:05:06 +0545

    added Lab 01

---
+++ Lab/Lab01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Hypotenus
+{
+    static double findHypotenuse(double side1, double side2){
+        double h = Math.Sqrt((side1 * side1) + (side2 * side2));
+        return h;
+    }
+
+    public static void Main(string[] args)
+    {
+        double side1, side2;
+
+        Console.WriteLine("Program to find hypotenuse of triangle.");
+
+        Console.Write("Side 1: ");
+        side1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Side 2: ");
+        side2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Hypotenuse: {0:F2}", findHypotenuse(side1, side2));
+    }
+}
